c7f5e265eb103b9e33c8231f92476547ec169916
k49977 <r.085.tk@i.softbank.jp>
2021-04-19 23:51:41 +0900

    rspec追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :first_name,  presence: true
   validates :family_name,  presence: true
-  validates :email,  presence: true
+  validates :email,  presence: true,uniqueness: true
   validates :password,  presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -30,6 +30,7 @@ RSpec.describe User, type: :model do
 
   it "姓、名、メール、パスワードがある場合、有効である" do
     # user = FactoryBot.build(:user)
+    # #spec/models/rails_helper.rbへの記載（config.include FactoryBot::Syntax::Methods）によって、FactoryBotを短縮
     user = build(:user)
     expect(user).to be_valid
   end
@@ -40,10 +41,30 @@ RSpec.describe User, type: :model do
     expect(user.errors[:first_name]).to include("can't be blank")
   end
 
-  it "姓がない場合、無効である"
-  it "メールアドレスがない場合、無効である"
-  it "重複したメールアドレスの場合、無効である"
-  it "パスワードがない場合、無効である"
+  it "姓がない場合、無効である" do
+    user = build(:user, family_name: nil)
+    user.valid?
+    expect(user.errors[:family_name]).to include("can't be blank")
+  end
+
+  it "メールアドレスがない場合、無効である" do
+    user = build(:user, email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+
+  it "重複したメールアドレスの場合、無効である" do
+    user1 = FactoryBot.create(:user)
+    user2 = FactoryBot.build(:user)
+    user2.valid?
+    expect(user2.errors[:email]).to include("has already been taken")
+  end
+
+  it "パスワードがない場合、無効である" do
+    user = FactoryBot.build(:user, password: nil)
+    user.valid?
+    expect(user.errors[:password]).to include("can't be blank")
+  end
 end
 
 
